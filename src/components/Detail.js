dd62d4ce32a2172bd54441838753f420aa352ab9
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-10-05 13:18:48 -0400

    add linters files

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -13,7 +13,7 @@ const Detail = (props) => {
 
 Detail.propTypes = {
   title: PropTypes.string.isRequired,
-  value: PropTypes.number.isRequired,
+  value: PropTypes.number,
 };
 
 export default Detail;
